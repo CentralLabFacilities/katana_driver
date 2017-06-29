267ef835e2bc70b423e2f383253d157904b47b99
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2017-06-29 16:03:36 +0200

    added ikfast plugin for tobi_3of4

---
+++ tobi_3of4_ikfast_left_arm_plugin/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_ikfast create_ikfast_moveit_plugin.py tobi_3of4 left_arm tobi_3of4_ikfast_left_arm_plugin /home/ffriese/catkin_ws/src/tobi_3of4_ikfast_left_arm_plugin/src/tobi_3of4_left_arm_ikfast_solver.cpp
